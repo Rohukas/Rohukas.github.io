ea19eeda5777c15af3fec9194bd6b41dae62d59f
Robi Rohumaa <rohumaarobi@gmail.com>
2020-01-23 13:12:04 +0200

    upl

---
+++ precache-manifest.0cefc6b47157269bacf6e9cd2db61a5b.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "29d7ac6875606f6eb11481d9413b755d",
+    "url": "/scamfinder/index.html"
+  },
+  {
+    "revision": "875dad8bb0c341268b72",
+    "url": "/scamfinder/static/css/2.ff8bd605.chunk.css"
+  },
+  {
+    "revision": "e283e325ae989267d9cc",
+    "url": "/scamfinder/static/css/main.d1b05096.chunk.css"
+  },
+  {
+    "revision": "875dad8bb0c341268b72",
+    "url": "/scamfinder/static/js/2.821cfde7.chunk.js"
+  },
+  {
+    "revision": "420368abb204bda0ccff1f3f356cb917",
+    "url": "/scamfinder/static/js/2.821cfde7.chunk.js.LICENSE"
+  },
+  {
+    "revision": "e283e325ae989267d9cc",
+    "url": "/scamfinder/static/js/main.a4674397.chunk.js"
+  },
+  {
+    "revision": "fbe90f24b5a6d894378e",
+    "url": "/scamfinder/static/js/runtime-main.27d2b83a.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/scamfinder/static/media/logo.5d5d9eef.svg"
+  }
+]);
